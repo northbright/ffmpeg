f6fcc9f6e47b295957a3cfb917ca5893979c3162
Frank Xu <frank.xu@northbright.com>
2026-09-08 18:03:05 +0800

    Add AddSoftSubtitle

--- srt/srt.go
+++ srt/srt.go
@@ -1,25 +1,28 @@
 package srt
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/exec"
 )
 
-type SubTitle struct {
+// Subtitle represents a subtitle in a SRT file.
+type Subtitle struct {
 	Start string
 	End   string
 	Text  string
 }
 
+// WriteFile writes subtitles to a SRT file.
 func WriteFile(srtFile string, subtitles []Subtitle) error {
 	if len(subtitles) == 0 {
-		return "", fmt.Errorf("empty subtitles")
+		return fmt.Errorf("empty subtitles")
 	}
 
 	content := ""
 	for i, s := range subtitles {
-		content = append(content, fmt.Sprintf("%d\n%s --> %s\n%s\n\n", i+1, s.Start, s.End, s.Text))
+		content += fmt.Sprintf("%d\n%s --> %s\n%s\n\n", i+1, s.Start, s.End, s.Text)
 	}
 
 	// Write subtitles in SRT file.
@@ -30,6 +33,65 @@ func WriteFile(srtFile string, subtitles []Subtitle) error {
 	return nil
 }
 
-func AddSoftSubtitlesCommand(videoFile string, subtitles map[string]string) (*exec.Cmd, error) {
-	return nil, nil
+// addSoftSubtitleArgs returns the arguments of [os/exec.Cmd] to add a soft subtitle track to a video with ffmpeg.
+// input: input video.
+// srtFile: srt file.
+// lang: three-letter [ISO 639-2 Code](e.g. "eng", "spa", "chi").
+// title: a user-friendly name for the subtitle track selection menu(e.g. "English", "Spanish", "Chinese").
+// output: output file.
+// overwrite: if overwrite if output exists.
+// [ISO 639-2 Code]: https://www.loc.gov/standards/iso639-2/php/code_list.php
+func addSoftSubtitleArgs(input, srtFile, lang, title, output string, overwrite bool) []string {
+	var args []string
+
+	if overwrite {
+		args = append(args, "-y")
+	}
+
+	args = append(args, "-i", input, "-i", srtFile)
+	args = append(args, "-c", "copy")
+	args = append(args, "-metadata:s:s:0", fmt.Sprintf("language=%s", lang))
+	args = append(args, "-metadata:s:s:0", fmt.Sprintf("title=%s", title))
+	args = append(args, output)
+
+	return args
+}
+
+// AddSoftSubtitleCommand returns the [os/exec.Cmd] to add a soft subtitle track to a video with ffmpeg.
+// input: input video.
+// srtFile: srt file.
+// lang: three-letter [ISO 639-2 Code](e.g. "eng", "spa", "chi").
+// title: a user-friendly name for the subtitle track selection menu(e.g. "English", "Spanish", "Chinese").
+// output: output file.
+// overwrite: if overwrite if output exists.
+func AddSoftSubtitleCommand(input, srtFile, lang, title, output string, overwrite bool) *exec.Cmd {
+	args := addSoftSubtitleArgs(input, srtFile, lang, title, output, overwrite)
+	return exec.Command("ffmpeg", args...)
+}
+
+// AddSoftSubtitleCommandContext is the context version of [AddSoftSubtitleCommand].
+func AddSoftSubtitleCommandContext(ctx context.Context, input, srtFile, lang, title, output string, overwrite bool) *exec.Cmd {
+	args := addSoftSubtitleArgs(input, srtFile, lang, title, output, overwrite)
+	return exec.Command("ffmpeg", args...)
+}
+
+// AddSoftSubtitle adds a soft subtitle track to a video with ffmpeg.
+// It returns the output from ffmpeg command.
+// input: input video.
+// srtFile: srt file.
+// lang: three-letter [ISO 639-2 Code](e.g. "eng", "spa", "chi").
+// title: a user-friendly name for the subtitle track selection menu(e.g. "English", "Spanish", "Chinese").
+// output: output file.
+// overwrite: if overwrite if output exists.
+func AddSoftSubtitle(ctx context.Context, input, srtFile, lang, title, output string, overwrite bool) (string, error) {
+	var cmd *exec.Cmd
+
+	if ctx == nil {
+		cmd = AddSoftSubtitleCommand(input, srtFile, lang, title, output, overwrite)
+	} else {
+		cmd = AddSoftSubtitleCommandContext(ctx, input, srtFile, lang, title, output, overwrite)
+	}
+
+	out, err := cmd.CombinedOutput()
+	return string(out), err
 }
--- srt/srt_test.go
+++ srt/srt_test.go
@@ -1,33 +1,60 @@
 package srt_test
 
 import (
+	"context"
 	"log"
 
 	"github.com/northbright/ffmpeg/srt"
 )
 
-func ExampleWriteFile() {
-	subtitles := []srt.SubTitle{
-		srt.SubTitle{"00:00:00,000", "00:00:05,090", "What's mimao's playing?"},
-		srt.SubTitle{"00:00:05,100", "00:00:08,200", "Does he realy like it?"},
+func ExampleAddSoftSubtitle() {
+	subtitles := []srt.Subtitle{
+		srt.Subtitle{"00:00:00,000", "00:00:05,090", "What's mimao's playing?"},
+		srt.Subtitle{"00:00:05,100", "00:00:08,200", "Does he realy like it?"},
 	}
 
-	f := "output/eng.srt"
-	if err := srt.WriteFile(f, subtitles); err != nil {
-		log.Printf("srt.WriteFile(%s) error: %v", f, err)
+	f1 := "../output/eng.srt"
+	if err := srt.WriteFile(f1, subtitles); err != nil {
+		log.Printf("srt.WriteFile(%s) error: %v", f1, err)
 		return
 	}
 
-	subtitles = []srt.SubTitle{
-		srt.SubTitle{"00:00:00,000", "00:00:05,090", "咪毛在玩啥？"},
-		srt.SubTitle{"00:00:05,100", "00:00:08,200", "他真的喜欢玩这个？"},
+	subtitles = []srt.Subtitle{
+		srt.Subtitle{"00:00:00,000", "00:00:05,090", "咪毛在玩啥？"},
+		srt.Subtitle{"00:00:05,100", "00:00:08,200", "他真的喜欢玩这个？"},
 	}
 
-	f := "output/chi.srt"
-	if err := srt.WriteFile(f, subtitles); err != nil {
-		log.Printf("srt.WriteFile(%s) error: %v", f, err)
+	f2 := "../output/chi.srt"
+	if err := srt.WriteFile(f2, subtitles); err != nil {
+		log.Printf("srt.WriteFile(%s) error: %v", f2, err)
 		return
 	}
+
+	ctx := context.Background()
+
+	// Add English subtitle.
+	input := "../videos/cat.MOV"
+	output := "../output/cat-eng.mkv"
+
+	out, err := srt.AddSoftSubtitle(ctx, input, f1, "eng", "English", output, true)
+	if err != nil {
+		log.Printf("str.AddSoftSubtitle() error: %v\noutput:\n%s", err, out)
+		return
+	}
+
+	log.Printf("str.AddSoftSubtitle() OK. output:\n%s", out)
+
+	// Add Chinese subtitle.
+	input = "../output/cat-eng.mkv"
+	output = "../output/cat-eng-chi.mkv"
+
+	out, err = srt.AddSoftSubtitle(ctx, input, f2, "chi", "Chinese", output, true)
+	if err != nil {
+		log.Printf("str.AddSoftSubtitle() error: %v\noutput:\n%s", err, out)
+		return
+	}
+
+	log.Printf("str.AddSoftSubtitle() OK. output:\n%s", out)
 
 	// Output:
 }
